966a4d8ddb79e5168d3c32be16e5b92c1052352b
NguyenHuyHoang34 <hoangnh2251@ut.edu.vn>
2026-01-10 04:33:35 +0700

    3. Implement TCP client connection

--- src/SecureChat.Server/Program.cs
+++ src/SecureChat.Server/Program.cs
@@ -1,21 +1,44 @@
-using Microsoft.Extensions.Logging;
-using SecureChat.Server;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Text;
 
 class Program
 {
-    static async Task Main(string[] args)
+    static void Main()
     {
-        using ILoggerFactory loggerFactory =
-            LoggerFactory.Create(builder =>
-            {
-                builder.AddConsole();
-            });
+        var cert = new X509Certificate2("server.pfx", "123456");
 
-        ILogger<ChatServer> logger =
-            loggerFactory.CreateLogger<ChatServer>();
+        TcpListener listener = new TcpListener(IPAddress.Any, 9000);
+        listener.Start();
 
-        ChatServer server = new ChatServer(9000, logger);
+        Console.WriteLine("Secure Chat Server running on port 9000");
 
-        await server.StartAsync();
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected");
+
+            _ = Task.Run(() => HandleClient(client, cert));
+        }
+    }
+
+    static void HandleClient(TcpClient client, X509Certificate2 cert)
+    {
+        using var sslStream = new SslStream(
+            client.GetStream(),
+            false
+        );
+
+        sslStream.AuthenticateAsServer(cert, false, false);
+
+        using var reader = new StreamReader(sslStream, Encoding.Unicode);
+        using var writer = new StreamWriter(sslStream, Encoding.Unicode) { AutoFlush = true };
+
+        string msg = reader.ReadLine()!;
+        Console.WriteLine("[SECURE] " + msg);
+
+        writer.WriteLine("Secure reply: " + msg);
     }
 }
